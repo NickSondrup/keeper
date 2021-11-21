1b853966e1aa7136bd09628d603d44e1d279b7fe
NickSondrup <nicholas.sondrup@gmail.com>
2021-11-20 21:38:04 -0700

    c

--- keeper.client/src/pages/AboutPage.vue
+++ keeper.client/src/pages/AboutPage.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="about">
+  <div class="container-fluid">
     <h1>This is the about page</h1>
-    <p>My name is Nicholas Sondrup, I am a software developer in Boise Idaho, I've always enjoyed creating and watching things grow. You can reach me at nicholas.sondrup@gmail.com </p>
     <img src="src/assests/img/IMG_9655.jpg" alt="">
+    <p>My name is Nicholas Sondrup, I am a software developer in Boise Idaho, I've always enjoyed creating and watching things grow. You can reach me at nicholas.sondrup@gmail.com </p>
   </div>
 </template>
 
